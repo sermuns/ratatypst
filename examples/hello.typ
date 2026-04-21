34c97b60f60ed95c41b928174daf3db220ac89cd
Samuel Åkesson <samak519@student.liu.se>
2026-04-21 17:00:36 +0200

    feat: create more interesting example

--- examples/hello.typ
+++ examples/hello.typ
@@ -1,7 +1,13 @@
 #import "../lib.typ": *
 
-#run(
-  80,
-  30,
-  "ssdddjkkkllllllll",
+#set text(size: 18pt)
+#show raw: set text(font: "JetBrains Mono")
+
+#align(
+  center,
+  run(
+    40,
+    20,
+    "jlhlhjhlhlhlhjhj",
+  ),
 )
--- src/app.rs
+++ src/app.rs
@@ -1,32 +1,89 @@
 use ratatui::prelude::*;
+use ratatui::symbols::border;
 use ratatui::widgets::Block;
-use ratatui::{layout::Position, widgets::Widget};
+use ratatui::widgets::Paragraph;
+use ratatui::widgets::Widget;
 
 pub struct App {
-    pos: Position,
+    focus: Focus,
+}
+
+#[derive(PartialEq, Debug)]
+enum Focus {
+    TopRight,
+    TopLeft,
+    Bottom,
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
 impl App {
     pub fn from_input_str(input: &str) -> Self {
-        let mut pos = Position::default();
+        let mut s = Self {
+            focus: Focus::Bottom,
+        };
         for c in input.chars() {
             match c {
-                'j' | 'w' => pos.y = pos.y.saturating_sub(1),
-                'h' | 'a' => pos.x = pos.x.saturating_sub(1),
-                'k' | 's' => pos.y = pos.y.saturating_add(1),
-                'l' | 'd' => pos.x = pos.x.saturating_add(1),
+                'j' | 'w' => s.change_focus(Direction::Down),
+                'h' | 'a' => s.change_focus(Direction::Left),
+                'k' | 's' => s.change_focus(Direction::Up),
+                'l' | 'd' => s.change_focus(Direction::Right),
                 _ => {}
             }
         }
-        Self { pos }
+        s
+    }
+
+    fn change_focus(&mut self, direction: Direction) {
+        match self.focus {
+            Focus::TopRight => match direction {
+                Direction::Down => self.focus = Focus::Bottom,
+                Direction::Left => self.focus = Focus::TopLeft,
+                _ => {}
+            },
+            Focus::TopLeft => match direction {
+                Direction::Down => self.focus = Focus::Bottom,
+                Direction::Right => self.focus = Focus::TopRight,
+                _ => {}
+            },
+            Focus::Bottom => match direction {
+                Direction::Up => self.focus = Focus::TopLeft,
+                Direction::Right => self.focus = Focus::TopRight,
+                _ => {}
+            },
+        }
     }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        Block::bordered().title("ratatypst").render(area, buf);
+        let block = Block::bordered().title("ratatypst");
+        (&block).render(area, buf);
 
-        let Position { x, y } = self.pos;
-        buf.set_string(x, y, "x", Style::default());
+        let [top, bottom] = block
+            .inner(area)
+            .layout(&Layout::vertical([Constraint::Fill(1); 2]));
+
+        let [top_left, top_right] = top.layout(&Layout::horizontal([Constraint::Fill(1); 2]));
+
+        for (area, focus) in [
+            (top_left, Focus::TopLeft),
+            (top_right, Focus::TopRight),
+            (bottom, Focus::Bottom),
+        ] {
+            let block = if focus == self.focus {
+                Block::bordered().border_set(border::THICK)
+            } else {
+                Block::bordered()
+            };
+            Paragraph::new(format!("{:?}", focus))
+                .block(block)
+                .render(area, buf);
+        }
     }
 }
